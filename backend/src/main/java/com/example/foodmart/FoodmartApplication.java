02b27edde7aeb775d20af435cf2340957d521e2a
Yschoi <dydtjr1717@gmail.com>
2021-05-28 01:59:12 +0900

    test
    123

--- backend/src/main/java/com/example/foodmart/FoodmartApplication.java
+++ backend/src/main/java/com/example/foodmart/FoodmartApplication.java
@@ -1,18 +1,1 @@
-package com.example.foodmart;
-
-import com.example.foodmart.File.FIleProperties;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@SpringBootApplication
-@EnableConfigurationProperties({
-        FIleProperties.class
-})
-public class FoodmartApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(FoodmartApplication.class, args);
-    }
-
-}
+package com.example.foodmart;import com.example.foodmart.File.FIleProperties;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.boot.context.properties.EnableConfigurationProperties;@SpringBootApplication@EnableConfigurationProperties({        FIleProperties.class})public class FoodmartApplication {    public static void main(String[] args) {        SpringApplication.run(FoodmartApplication.class, args);    }}
